855c726ad909ce54569b38c17fadd57174416231
Felipe Marinho <felipmarinho@gmail.com>
2020-01-08 06:48:38 -0300

    Number: alguns cuidados

---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,13 @@
+// Number: alguns cuidados
+console.log(7 / 0); // Infinity
+console.log("10" / 2); // = 5
+console.log("10.2" / 2); // = 5.1
+console.log("10,2" / 2); // = NaN
+console.log("Show!" * 2); // = NaN
+console.log(0.1 + 0.7); // = 0.7999999999999999
+// console.log(10.toString()); // = Excepetion
+console.log((10.345).toFixed(2)); // = 10.35
+
+
+
+
