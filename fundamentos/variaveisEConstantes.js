dc17ace4761fa5a26508a8e8a2a4f371ad98c5a5
Felipe Marinho <felipmarinho@gmail.com>
2020-01-08 05:56:51 -0300

    O básico de var, let e const

---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,25 @@
+// O básico de var, let e const
+
+
+var a = 3 
+let b = 4 // forma mais moderna indicado a usar assim.
+
+var a = 30
+b = 40
+
+console.log(a, b);
+
+a = 300
+b = 400
+
+console.log(a, b);
+
+const c = 5
+// c = 50; // da erro
+console.log(c);
+
+/**
+ * Use rotulos com bons nomes
+ * 
+ * De preferência por usar constante, se fd não tem motivo para mudar use constante.
+ */
