e11d391e470627385f18d18301a77f97ed9a914a
Felipe Marinho <felipmarinho@gmail.com>
2020-01-08 05:45:41 -0300

    Comentário de código

---
+++ fundamentos/comentario.js
@@ -0,0 +1,26 @@
+// Comentário de código
+
+// Comentário de uma linha
+console.log('Linha 1');
+
+/*
+	comentário de
+	multiplas linhas
+*/
+console.log('Linhas 2');
+
+
+/**
+ * Comentário de
+ * múltiplas linhas
+ */
+console.log('Linhas');
+
+/**
+ * Informações adicionais
+ * 
+ * 1) Comentário é utilizado próximo ao o que se quer ser comentádo.
+ * 2) Não faça comentário desnecessários
+ * 3) Código legível invés de fazer comentários.
+ * 
+ */
