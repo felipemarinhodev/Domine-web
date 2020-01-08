c70c42f8da2ba9ed08e81fc735ef5830b66bc654
Felipe Marinho <felipmarinho@gmail.com>
2020-01-08 06:23:39 -0300

    Tipos em JavaScript: Number

---
+++ fundamentos/numeros.js
@@ -0,0 +1,23 @@
+// Tipos em JavaScript: Number
+const peso1 = 1.0;
+const peso2 = Number('2.0')
+
+console.log(peso1, peso2);
+console.log(Number.isInteger(peso1));
+console.log(Number.isInteger(peso2));
+
+const avaliacao1 = 9.871
+const avaliacao2 = 6.871
+
+const total = avaliacao1 * peso1 + avaliacao2 * peso2
+const media = total / (peso1 + peso2)
+
+console.log('media: ', media);
+console.log('media formatada: ', media.toFixed(2));
+
+console.log('Média toString():', media.toString());
+console.log('Média toString():', media.toString(2)); // em binário.
+
+console.log('media =', typeof media);
+console.log('Number =', typeof Number);
+
