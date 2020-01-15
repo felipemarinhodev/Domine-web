b764030f02daa0fe53660ef129b1de96623bf737
Felipe Marinho <felipmarinho@gmail.com>
2020-01-15 05:02:43 -0300

    Operadores: Atribuição

---
+++ fundamentos/atribuicao.js
@@ -0,0 +1,17 @@
+const a = 7
+let b = 3
+
+b += a // atribuição aditiva | b = b + a
+console.log(b);
+
+b -= 4 // atribuição subtrativa | b = b - 4
+console.log(b);
+
+b *= 2 // atribuição multiplicativa | b = b * 2
+console.log(b);
+
+b /= 2 // atribução divisiva | b = b / 2
+console.log(b);
+
+b %= 2 // b = b % 2
+console.log(b);
