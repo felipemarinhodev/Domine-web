6e30f4082e3ade0c9019305a151bfcb1e7e91caf
Felipe Marinho <felipmarinho@gmail.com>
2020-01-15 04:55:16 -0300

    Notação ponto

---
+++ fundamentos/notacaoPonto.js
@@ -0,0 +1,21 @@
+console.log(typeof console);
+
+console.log(Math.ceil(6.1)); // arredonda para cima
+
+const obj1 = {}
+obj1.nome = 'Bola'
+// obj1['nome'] = 'Bola2'
+console.log(obj1.nome);
+
+function Obj(nome) {
+	this.nome = nome; // o this.<nome variavel> cria uma variavel que é publica e pode ser acessada de qlq lugar.
+	this.exec = function () {
+		console.log('Exec...');
+	}
+}
+
+const obj2 = new Obj('Cadeira');
+const obj3 = new Obj('Mesa');
+console.log(obj2.nome);
+console.log(obj3.nome);
+obj3.exec()
